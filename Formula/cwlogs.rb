5f1bf9308f7db5068180cfbfc3eeb82e23d217c1
Harvey Wang <harvey.wanghy@gmail.com>
2025-05-21 16:52:22 -0700

    Update cwlogs formula to v0.3.3

--- Formula/cwlogs.rb
+++ Formula/cwlogs.rb
@@ -1,9 +1,9 @@
 class Cwlogs < Formula
   desc "AWS CloudWatch Log Viewer with HTML output, alias & highlight support"
   homepage "https://github.com/HarveyGG/aws-cloudwatch-viewer-cli"
-  url "https://github.com/HarveyGG/aws-cloudwatch-viewer-cli/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "903464cc1b0575db1ef710d5161fa6292f622ff9a486dd87b278b4fb104d4f0d"
-  version "0.3.2"
+  url "https://github.com/HarveyGG/aws-cloudwatch-viewer-cli/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "947929681cdb82feda6b99a867fe35f0f62d5ceca91abe6275ec96508d308918"
+  version "0.3.3"
 
   def install
     bin.install "bin/cwlogs"
